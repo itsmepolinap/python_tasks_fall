15859255f1b9cac95a6a61c2af25f8653805ca88
itsmepolinap <polina.pavlichuk2002@yandex.ru>
2024-11-23 14:47:51 +0300

    done

--- 1_strings/3_maximum_number_of_words/main.py
+++ 1_strings/3_maximum_number_of_words/main.py
@@ -11,4 +11,14 @@ pytest ./3_maximum_number_of_words/test.py
 
 def get_max_number_of_words_from_sentences(sentences: list[str]) -> int:
     """Пишите ваш код здесь."""
+    # spisok = []
+    # for i in sentences:
+    #     spisok.append(len(i.split()))
+    # return max(spisok)
 
+    max_num = 0
+    for i in sentences:
+        cur_num = len(i.split())
+        if cur_num > max_num:
+            max_num = cur_num
+    return max_num
