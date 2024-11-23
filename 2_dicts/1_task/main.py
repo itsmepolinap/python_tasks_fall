2cae46017693c3983e76fdf511d17fda21a6abc1
itsmepolinap <polina.pavlichuk2002@yandex.ru>
2024-11-23 14:48:19 +0300

    done

--- 2_dicts/1_task/main.py
+++ 2_dicts/1_task/main.py
@@ -1,3 +1,4 @@
+import decimal
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -19,8 +20,25 @@ def get_employees_info() -> list[str]:
 
 def get_parsed_employees_info() -> list[dict[str, int | str]]:
     """Функция парсит данные, полученные из внешнего API и приводит их к стандартизированному виду."""
-    _ = get_employees_info()
+    employees_info = get_employees_info()
     parsed_employees_info = []
+    for emp in employees_info:
+        employee_card = {}
+        emp = emp.split()
+        keys_list = emp[::2]
+        values_list = emp[1::2]
+        for key, value in zip(keys_list, values_list):
+            #id:int, name, last_name, age:int, salary:decimal, position
+            if key in ["id", "age"]:
+                employee_card[key] = int(value)
+            elif key == "salary":
+                employee_card[key] = decimal.Decimal(value)
+            elif key in ["name", "last_name", "position"]:
+                employee_card[key] = value
+
+        parsed_employees_info.append(employee_card)
+    
 
     # Ваш код ниже
     return parsed_employees_info
+
