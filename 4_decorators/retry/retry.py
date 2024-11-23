d0947c6f4f3fd3f1e37425cdea8271504d59ae25
itsmepolinap <polina.pavlichuk2002@yandex.ru>
2024-11-23 14:49:49 +0300

    done

--- 4_decorators/retry/retry.py
+++ 4_decorators/retry/retry.py
@@ -1,2 +1,23 @@
+from datetime import timedelta
+from time import sleep
 # реализуйте декоратор вида @retry(count: int, delay: timedelta, handled_exceptions: tuple[type(Exceptions)])
 
+def retry(count: int, delay: timedelta, handled_exceptions: tuple[Exception] = None):
+    if count < 1:
+        raise ValueError
+    if handled_exceptions is None:
+        handled_exceptions = (Exception, )
+    def decorator(func):
+        def wrap(*args,**kwargs):
+            for attempt in range(count):
+                try:
+                    return func(args,kwargs)
+                except handled_exceptions:
+                    if attempt + 1 == count:
+                        raise 
+                sleep(delay.total_seconds())
+        return wrap     
+    return decorator
+
+
+                
