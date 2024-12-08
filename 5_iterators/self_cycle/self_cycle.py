d59c15c3a1b2329c3ff530ea62ce9b0a5d8f7529
itsmepolinap <polina.pavlichuk2002@yandex.ru>
2024-12-08 13:51:24 +0300

    done

--- 5_iterators/self_cycle/self_cycle.py
+++ 5_iterators/self_cycle/self_cycle.py
@@ -5,8 +5,34 @@ T = TypeVar("T")
 
 def cycle(obj: Iterable[T]) -> Generator[T, None, None]:
     """Пишите ваш код здесь."""
+    copy_of_iterable = []
+    for elem in obj:
+        copy_of_iterable.append(elem)
+        yield elem
 
+    while True:
+        yield from copy_of_iterable
 
 class Cycle:
     def __init__(self, obj: Iterable[T]):
         """Реализуйте класс"""
+        self.obj = obj
+        self.current_iter = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.current_iter:
+            self.current_iter = iter(self.obj)
+            return next(self.current_iter)
+        else:
+            try:
+                return next(self.current_iter)
+            except StopIteration:
+                self.current_iter = iter(self.obj)
+                return next(self.current_iter)
+
+if __name__ == '__main__':
+    for i in Cycle([1, 2, 3, 4]):
+        print(i)
